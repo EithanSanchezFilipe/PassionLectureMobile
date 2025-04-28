bcac69a8b865da2507dfcc7312f2d311e3be5f66
EithanSanchezFilipe <150446486+EithanSanchezFilipe@users.noreply.github.com>
2025-04-28 14:33:51 +0200

    feat: implement BookViewModel and bind it to Book.xaml

---
+++ ReadME/ReadME/ViewModel/BookViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Net.Http;
+using System;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace ReadME.ViewModel
+{
+    public partial class BookViewModel: ObservableObject
+    {
+        static HttpClient client = new HttpClient();
+
+        static async void GetBook() {
+            client.GetAsync();
+        }
+    }
+}
